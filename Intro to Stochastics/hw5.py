4a7c3cedfd4464f1c0f6b0e49137387ac57f432e
chuanhuan <studyhuan@gmail.com>
2021-10-31 02:01:02 +0800

    20211030

--- Intro to Stochastics/hw5.py
+++ Intro to Stochastics/hw5.py
@@ -2,10 +2,28 @@
 #%%
 import pandas as pd 
 import numpy as np 
+import math
 #%%
 
-# q1
+# q1-a
 
-for i in range(0,7):
-    print('the i is',i)
+solution = 0
+for n in range(1,7):
+    tmp = math.comb(8,n-1)/math.comb(10,n)
+    print('the i is',tmp)
+    solution=tmp/6+solution
+print(solution)
+
+# q1-b expectation of x
+
+solution = 0
+for x in range(1,11):
+    for n in range(1,7):
+        tmp = x*math.comb(10-x,n-1)/math.comb(10,n)
+        print('the i is',tmp)
+        solution=tmp/6+solution
+print(solution)
+
+
+
 # %%
