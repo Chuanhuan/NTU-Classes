a149cc6a5b66d3c3e329301a677d93879d59c902
chuanhuan <studyhuan@gmail.com>
2024-09-01 16:57:31 +0800

    new file:   XAI/High_var_index-2336753849_dc82a349b4.png 	new file:   XAI/High_var_index-330964178_f0971c678b.png 	new file:   XAI/Surrogate_image-2336753849_dc82a349b4.png 	new file:   XAI/Surrogate_image-330964178_f0971c678b.png 	modified:   XAI/grad-cam-heatmap.jpg 	new file:   XAI/grdd-cam-map-2336753849_dc82a349b4.jpg 	new file:   XAI/grdd-cam-map-330964178_f0971c678b.jpg 	modified:   XAI/grdd-cam-map.jpg 	new file:   XAI/orig-image-2336753849_dc82a349b4.jpg 	new file:   XAI/orig-image-330964178_f0971c678b.jpg 	new file:   XAI/vgg_grad_cam.ipynb 	modified:   XAI/vgg_grad_cam.py

--- XAI/vgg_grad_cam.py
+++ XAI/vgg_grad_cam.py
@@ -9,6 +9,9 @@ import matplotlib.pyplot as plt
 import numpy as np
 import os
 from PIL import Image
+import torch.distributions as dist
+import torch.nn.functional as F
+import cv2
 
 # %%
 # use the ImageNet transformation
@@ -96,27 +99,42 @@ vgg.eval()
 # get the image from the dataloader
 # img, _ = next(iter(dataloader))
 
-image_dir = os.path.expanduser("~/Documents/imagenet_images/elephant/")
+image_dir = os.path.expanduser("~/Documents/imagenet_images/piggery/")
 image_files = [
     os.path.join(image_dir, file)
     for file in os.listdir(image_dir)
     if file.endswith(".jpg")
 ]
-img = Image.open(image_files[1])
-img = transform(img).unsqueeze(0)
 
-# get the most likely prediction of the model
 # Load the ImageNet class names
 with open("./imagenet-classes.txt") as f:
     class_names = [line.strip() for line in f.readlines()]
-pred = vgg(img).argmax(dim=1)
 
-print(f"Prediction: {class_names[pred]}")
+image_file = image_files[2]
+img = Image.open(image_file)
+img = transform(img).unsqueeze(0)
+
+# get the most likely prediction of the model
+pred_index = vgg(img).argmax(dim=1)
+
+print(f"Prediction: {pred_index}, {class_names[pred_index]}")
+# Get the top 5 predictions of the model
+top_pred_values, top_pred_indices = vgg(img).topk(5)
+
+# Print the top 5 predictions and their corresponding classes
+top_pred_indices = top_pred_indices.detach().cpu().numpy().flatten()
+top_pred_values = F.softmax(vgg(img), dim=1)[:, top_pred_indices]
+top_pred_values = top_pred_values.detach().cpu().numpy().flatten()
+for i in range(5):
+    print(
+        f"Prediction {i+1}: {top_pred_indices[i]}, {top_pred_values[i]*100 :.2f}%, {class_names[top_pred_indices[i]]}"
+    )
+
 
 # %%
 # get the gradient of the output with respect to the parameters of the model
 pred = vgg(img)
-pred[:, 386].backward()
+pred[:, pred_index].backward()
 
 # pull the gradients out of the model
 gradients = vgg.get_activations_gradient()
@@ -148,16 +166,216 @@ plt.matshow(heatmap.squeeze())
 plt.savefig("./grad-cam-heatmap.jpg")
 
 # %%
-import cv2
 
+# Get the base name of the image file (i.e., the name without the directory path)
+image_name_with_ext = os.path.basename(image_file)
+
+# Split the base name into name and extension
+image_name, image_ext = os.path.splitext(image_name_with_ext)
 # img = cv2.imread("./data/Elephant/data/05fig34.jpg")
-img = cv2.imread(image_files[1])
-heatmap = cv2.resize(heatmap, (img.shape[1], img.shape[0]))
-heatmap = np.uint8(255 * heatmap)
-heatmap = cv2.applyColorMap(heatmap, cv2.COLORMAP_JET)
+img = cv2.imread(image_file)
+cv2.imwrite(f"./orig-image-{image_name}.jpg", img)
 
-superimposed_img = heatmap * 0.4 + img
-cv2.imwrite("./grdd-cam-map.jpg", superimposed_img)
+# %%
+
+# Convert the PyTorch tensor to a NumPy array
+heatmap_np = heatmap.numpy()
+
+# Resize the heatmap
+heatmap_resized = cv2.resize(heatmap_np, (img.shape[1], img.shape[0]))
+heatmap_resized = np.uint8(255 * heatmap_resized)
+heatmap_resized = cv2.applyColorMap(heatmap_resized, cv2.COLORMAP_JET)
+
+superimposed_img = heatmap_resized * 0.4 + img
+print(image_name, image_ext)
+cv2.imwrite(f"./grdd-cam-map-{image_name}.jpg", superimposed_img)
+
+# heatmap = cv2.resize(heatmap, (img.shape[1], img.shape[0]))
+# heatmap = np.uint8(255 * heatmap)
+# heatmap = cv2.applyColorMap(heatmap, cv2.COLORMAP_JET)
+#
+# superimposed_img = heatmap * 0.4 + img
+# print(image_name, image_ext)
+# cv2.imwrite(f"./grdd-cam-map-{image_name}.jpg", superimposed_img)
+
+# %%
+
+
+image_file = image_files[2]
+img = Image.open(image_file)
+img = transform(img).unsqueeze(0)
+
+model = vgg19(weights=True)
+
+
+def ll_gaussian(y, mu, log_var):
+    sigma = torch.exp(0.5 * log_var)
+    return -0.5 * torch.log(2 * np.pi * sigma**2) - (1 / (2 * sigma**2)) * (y - mu) ** 2
+
+
+class VI(nn.Module):
+    def __init__(self):
+        super().__init__()
+
+        self.q_mu = nn.Sequential(
+            nn.Linear(3 * 224 * 224, 20),
+            nn.ReLU(),
+            nn.Linear(20, 10),
+            nn.ReLU(),
+            nn.Linear(10, 3 * 224 * 224),
+        )
+        self.q_log_var = nn.Sequential(
+            nn.Linear(3 * 224 * 224, 20),
+            nn.ReLU(),
+            nn.Linear(20, 10),
+            nn.ReLU(),
+            nn.Linear(10, 3 * 224 * 224),
+        )
+
+    def reparameterize(self, mu, log_var):
+        # Define the minimum and maximum values for log_var
+        min_log_var = -10
+        max_log_var = 10
+
+        # Clip log_var to the specified range
+        clipped_log_var = torch.clamp(log_var, min_log_var, max_log_var)
+
+        # Compute sigma
+        sigma = torch.exp(0.5 * clipped_log_var) + 1e-5
+        # print(torch.sum(torch.isnan(sigma)))
+        # std can not be negative, thats why we use log variance
+        # sigma = torch.exp(0.5 * log_var) + 1e-5
+        eps = torch.randn_like(sigma)
+        return mu + sigma * eps
+
+    def forward(self, x):
+        mu = self.q_mu(x)
+        log_var = self.q_log_var(x)
+        return self.reparameterize(mu, log_var), mu, log_var
+
+
+def elbo(x_pred, X, mu, log_var, model=model, predicted=pred_index.item()):
+    # HACK: use the CNN model predition as the input
+    model.eval()
+    input = x_pred.view(1, 3, 224, 224)
+    # Forward pass
+    outputs = model(input)
+    # print(torch.sum(torch.isnan(outputs)))
+    eps = 1e-8  # small constant
+
+    tmp = F.softmax(outputs, dim=1)[:, predicted] + eps
+    log_p_y = torch.log(tmp)
+    # HACK: use eps for variance, as we want
+    # Define the minimum and maximum values for log_var
+    min_log_var = -10
+    max_log_var = 10
+
+    # Clip log_var to the specified range
+    clipped_log_var = torch.clamp(log_var, min_log_var, max_log_var)
+
+    # Compute sigma
+    sigma = (clipped_log_var.exp() + 1e-8) ** 0.5
+
+    # likelihood of observing y given Variational mu and sigma
+    likelihood = dist.Normal(mu, sigma).log_prob(X)
+    # sigma = log_var.exp() ** 0.5
+    # # likelihood of observing y given Variational mu and sigma
+    # likelihood = dist.Normal(mu, sigma).log_prob(X)
+
+    # prior probability of x_pred
+    log_prior = dist.Normal(0, 1).log_prob(x_pred)
+
+    # variational probability of x_pred
+    log_p_q = dist.Normal(mu, sigma).log_prob(x_pred)
+
+    # by taking the mean we approximate the expectation
+    return (log_p_y + likelihood + log_prior - log_p_q).mean() + eps
+
+
+def det_loss(x_pred, X, mu, log_var, model=model, predicted=pred_index.item()):
+    return -elbo(x_pred, X, mu, log_var, model, predicted)
+
+
+# %%
+"""### Training VI"""
+
+# NOTE: Train the VI model
+# Check if CUDA is available and set PyTorch to use GPU or CPU accordingly
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+model = vgg19(weights=True).to(device)  # Move the VGG model to the device
+
+# Move the model to the GPU
+m = VI().to(device)
+
+optim = torch.optim.Adam(m.parameters(), lr=0.005)
+
+epochs = 1000
+for epoch in range(epochs + 1):
+    # Move the data to the GPU
+    X = img.view(3 * 224 * 224).clone().to(device)
+    optim.zero_grad()
+    x_pred, mu, log_var = m(X)
+
+    loss = det_loss(x_pred, X, mu, log_var, model, pred_index.item())
+
+    if epoch % 10 == 0:
+        print(f"epoch: {epoch}, loss: {loss}")
+
+    loss.backward()
+    optim.step()
+
+# %%
+
+with torch.no_grad():
+    X = img.view(3 * 224 * 224).clone()
+    x_pred, mu, log_var = m(X)
+    print(torch.abs(x_pred - X).mean())
+
+new_image = mu.view(1, 3, 224, 224)
+new_image = (new_image - new_image.min()) / (new_image.max() - new_image.min())
+predicted = torch.argmax(F.softmax(model(new_image), dim=1))
+print(
+    f"True y = {pred_index.item()}, the highest probability index: {predicted.max():.5f}"
+)
+pred_prob = F.softmax(model(new_image), dim=1)[:, predicted]
+print(f"New image full model prediction: {pred_prob.item()}")
+new_image = new_image.permute(0, 2, 3, 1)
+plt.imshow(new_image.squeeze(0).detach().numpy())
+plt.title(
+    f"Pred {pred_index.item()} Surrogate model with prediction: {pred_prob.item():.3f}"
+)
+plt.savefig(f"Surrogate_image-{image_name}.png")
+plt.show()
+plt.clf()
+
+# %%
+
+X = img.view(3 * 224 * 224).clone().to(device)
+X = (X - X.min()) / (X.max() - X.min())
+
+print(f"variance: {log_var.exp().max()}")
+highest_var = log_var.exp().max()
+k = 0.8
+high_var_index = np.where(log_var.view(3, 224, 224).exp() > highest_var * k)
+
+plt.imshow(X.view(3, 224, 224).permute(1, 2, 0).detach().numpy())
+# plt.scatter(high_var_index[1], high_var_index[0], s=10, c="red")
+# Assume log_var is a tensor and you compute its exponential
+exp_values = log_var.view(3, 224, 224).exp()
+
+# Flatten the tensor to 1D for scatter plot
+exp_values_flatten = exp_values[high_var_index[0], high_var_index[1], high_var_index[2]]
+
+
+# Scatter plot with colors corresponding to exp_values
+plt.scatter(high_var_index[2], high_var_index[1], s=10, c=exp_values_flatten)
+
+# Add a colorbar to show the mapping from colors to values
+plt.title(f"Pred {pred_index.item()} High Variance Index(> {k})")
+plt.colorbar(label="exp(log_var)")
+plt.savefig(f"High_var_index-{image_name}.png")
+plt.show()
+plt.clf()
 
 
 # %%
